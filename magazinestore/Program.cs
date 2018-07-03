28e7c4021fb4528ed649e4fed19ce79d51f6d752
Alexandre Konioukhov <alexan1@gmail.com>
2018-07-02 22:53:19 -0400

    token

--- magazinestore/Program.cs
+++ magazinestore/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace magazinestore
 {
@@ -6,7 +9,19 @@ namespace magazinestore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var result = GetTokenAsync().Result;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }        
+        public static async Task<string> GetTokenAsync()
+        {
+            var client = new HttpClient();
+            var baseapi = "http://magazinestore.azurewebsites.net/";
+            var tokenapi = "api/token";
+            var url = baseapi + tokenapi;
+            var json = await client.GetStringAsync(url);
+            var result = JObject.Parse(json)["token"].ToString();            
+            return result;
         }
     }
 }
