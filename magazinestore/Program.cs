557ccd6f99f121724434317e3d87248675de1ee2
Alexandre Konioukhov <alexan1@gmail.com>
2018-07-02 23:17:41 -0400

    GetSubscribers

--- magazinestore/Program.cs
+++ magazinestore/Program.cs
@@ -9,18 +9,27 @@ namespace magazinestore
     {
         static void Main(string[] args)
         {
-            var result = GetTokenAsync().Result;
-            Console.WriteLine(result);
-            Console.ReadLine();
-        }        
-        public static async Task<string> GetTokenAsync()
-        {
-            var client = new HttpClient();
             var baseapi = "http://magazinestore.azurewebsites.net/";
             var tokenapi = "api/token";
             var url = baseapi + tokenapi;
+
+            var result = GetTokenAsync(url).Result;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }        
+        public static async Task<string> GetTokenAsync(string url)
+        {
+            var client = new HttpClient();            
             var json = await client.GetStringAsync(url);
             var result = JObject.Parse(json)["token"].ToString();            
+            return result;
+        }
+
+        public static async Task<string> GetSubscribers(string url)
+        {
+            var client = new HttpClient();
+            var json = await client.GetStringAsync(url);
+            var result = JObject.Parse(json)["token"].ToString();
             return result;
         }
     }
